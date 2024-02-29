48444a43715f8d6655ddc11acc232e5d033e2603
Cakraawala <kyuackerman30@gmail.com>
2024-02-29 14:52:27 +0700

    Authentication

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,3 +1,13 @@
-abstract class UserRepository{
-  
+import 'models/models.dart';
+
+abstract class UserRepository {
+  Stream<MyUser?> get user;
+
+	Future<MyUser> signUp(MyUser myUser, String password);
+
+	Future<void> setUserData(MyUser user);
+
+	Future<void> signIn(String email, String password);
+
+	Future<void> logOut();
 }
